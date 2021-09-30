75553401df09f6daac88c6a97a1a8c73c3bead26
Ritwick-Roy <ritwickroy2002@gmail.com>
2021-09-30 19:37:49 +0530

    async/await

--- sandbox.js
+++ sandbox.js
@@ -1,11 +1,22 @@
-//fetch api
+//async and await
+const getTodos = async () => {
+    const response = await fetch('todos/luigi.json');
+    if(response.status!==200)
+    {
+        throw new Error('Could not fetch the data!');
+    }
+    const data = await response.json();
+    return data;
+};
 
-fetch('todos/luigi.json').then(response=>{
-    // const luigi=JSON.parse(data);
-    console.log(response);
-    return response.json();  //response.json returns a promise
-}).then(data=>{
-    console.log(data);
-}).catch(err=>{
-    console.log('Error occured: ',err);
-})
+// const test = getTodos();
+// console.log(test);
+
+console.log(1);
+console.log(2);
+getTodos()
+    .then( dat => console.log('Resolved: ',dat))
+    .catch(err=>console.log('Error: ',err.message));
+
+console.log(3);
+console.log(4);
